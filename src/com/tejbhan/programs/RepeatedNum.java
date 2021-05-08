50d55158670239129ac21ce18a01f0fa5199407b
tejbhansahu <tejbhansahu0.ts@gmail.com>
2021-05-08 16:50:46 +0530

    Update RepeatedNum.java

--- src/com/tejbhan/programs/RepeatedNum.java
+++ src/com/tejbhan/programs/RepeatedNum.java
@@ -1,5 +1,21 @@
 package com.tejbhan.programs;
 
+//Question 3
+
 public class RepeatedNum {
+	
+	public static void main(String[] args) {      
+        
+        int [] arr = new int [] {1, 2, 3, 4, 2, 7, 8, 8, 3};   
+          
+        System.out.println("Duplicate elements in given array: ");  
+        
+        for(int i = 0; i < arr.length; i++) {  
+            for(int j = i + 1; j < arr.length; j++) {  
+                if(arr[i] == arr[j])  
+                    System.out.println(arr[j]);  
+            }  
+        }  
+    }  
 
 }
